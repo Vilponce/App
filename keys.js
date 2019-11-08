b18acfabf23b48efb6f4f8f98a43a1ea81512043
unknown <oscardavid2094@gmail.com>
2019-11-07 21:08:39 -0300

    siete nov

--- keys.js
+++ keys.js
@@ -1,3 +1,3 @@
-module.export = {
+module.exports = {
     mongoURI: "mongodb+srv://vilma123:100caramelos@cluster0-uo7zw.gcp.mongodb.net/test?retryWrites=true&w=majority"
-}   
+}
--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const mongoUrl = require("./keys.js");
 
 const app = express();
 
 //bodyparsen Middleware
-app.use(bodyParser/json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//db config
-const db = require(./config/keys).mongoURI;
 
-//connect to mongo
+app.get('/', (req, res) => res.send ('Hello From Express'));
+
+
+// //connect to mongo
 mongoose
-.connect(db)
+.connect(mongoUrl.mongoURI)
 .then (()=> console.log("MongoDB Connected..."))
 .catch(err => console.log(err) );
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("Server started on port ${port}"));
+app.listen(port, () => console.log(`Server started on port ${port}`));
